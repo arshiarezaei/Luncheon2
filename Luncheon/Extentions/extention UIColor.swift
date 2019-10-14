f5030741a8a1ba4c3df4920ccb5a4be5d46a24bf
arshiya rezaie <arshiyarezaie@gmail.com>
2019-10-14 19:51:43 +0330

    MainTabBarViewController class and UIColor Extention created profile icon added to Assets.xcassets

---
+++ Luncheon/Extentions/extention UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  extention UIColor.swift
+//  Luncheon
+//
+//  Created by arshiya  on 7/22/1398 AP.
+//  Copyright © 1398 AP Pouyesh system Saba. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    public static let  luncheonLogoTextColor = UIColor(red: 171/255, green: 30/255, blue: 35/255, alpha: 1)
+}
---
+++ Luncheon/MainTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarViewController.swift
+//  Luncheon
+//
+//  Created by arshiya  on 7/22/1398 AP.
+//  Copyright © 1398 AP Pouyesh system Saba. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

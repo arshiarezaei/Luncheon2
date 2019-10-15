693599688652166cd6f0d49d9a3c60df6496e4de
arshiya rezaie <arshiyarezaie@gmail.com>
2019-10-15 19:04:44 +0330

    FCRFoodCollectionView initializers implemented

--- Luncheon/food court/FCRFood/FCRFoodCollectionView.swift
+++ Luncheon/food court/FCRFood/FCRFoodCollectionView.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class FCRFoodCollectionView: UICollectionView {
 
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.

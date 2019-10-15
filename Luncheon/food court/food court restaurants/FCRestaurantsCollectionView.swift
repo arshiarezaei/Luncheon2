28fbfe33321d4c05fe8c225c9771f799c86f386a
arshiya rezaie <arshiyarezaie@gmail.com>
2019-10-15 14:28:19 +0330

    bottom border added to FCRestaurantsCollectionView

--- Luncheon/food court/food court restaurants/FCRestaurantsCollectionView.swift
+++ Luncheon/food court/food court restaurants/FCRestaurantsCollectionView.swift
@@ -15,17 +15,28 @@ class FCRestaurantsCollectionView: UICollectionView {
     
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        let y = bounds.maxY - 1
+        let minX = bounds.minX
+        let maxX = bounds.maxX
+        let lineColor = UIColor(red: 236/255, green: 235/255, blue: 235/255, alpha: 1).cgColor
+        context.setStrokeColor(lineColor)
+        context.setLineWidth(2.0)
+        context.move(to: CGPoint(x: minX, y: y))
+        context.addLine(to: CGPoint(x: maxX, y: y))
+        context.strokePath()
+        
     }
-    */
-
+    
+    
 }

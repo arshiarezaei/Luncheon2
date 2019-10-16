3d58fca01e5788ccdc232e9d8cd77de2e12c930a
arshiya rezaie <arshiyarezaie@gmail.com>
2019-10-17 00:02:39 +0330

    Utilities class created .convert to Persian numbers and symbols method implemented in Utility class

---
+++ Luncheon/Utilities/Utilities.swift
@@ -0,0 +1,21 @@
+//
+//  Utilities.swift
+//  Luncheon
+//
+//  Created by arshiya  on 7/24/1398 AP.
+//  Copyright © 1398 AP Pouyesh system Saba. All rights reserved.
+//
+
+import Foundation
+
+
+class Utilities {
+    private static let numbers = ["0":"۰","1":"۱","2":"۲","3":"۳","4":"۴","5":"۵","6":"۶","7":"۷","8":"۸","9":"۹",".":"/"]
+    func convertToPersianNumber(number:Double) -> String {
+        var result:String = String(number)
+        for (key,value) in Utilities.numbers  {
+            result = result.replacingOccurrences(of: key, with: value)
+        }
+        return result
+    }
+}
